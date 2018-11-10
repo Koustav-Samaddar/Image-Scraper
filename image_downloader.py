4ff56bfafc6dd04c50a52b75b707d5caf68b3f93
Koustav <koustav201197@gmail.com>
2018-11-10 16:32:33 -0500

    Created skeleton for the image_downloader python module.

---
+++ image_downloader.py
@@ -0,0 +1,14 @@
+
+import os
+
+
+def save_images_from_urls(urls, directory, prefix):
+	pass
+
+
+def dl_image_from_google(search, directory='F:/Images/'):
+	pass
+
+
+if __name__ == '__main__':
+	print("Hello Scrapers!")
