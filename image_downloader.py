05986432c6ae3b1d805d0c6e29c7efd79b8f10dd
Koustav <koustav201197@gmail.com>
2018-11-10 16:34:34 -0500

    Completed save_images_from_urls method. This should take a list of image urls and info about where and how to save the image files, and use that to save all the images.

--- image_downloader.py
+++ image_downloader.py
@@ -1,9 +1,25 @@
 
 import os
 
+from urllib.error import HTTPError, URLError
+from urllib.request import urlretrieve, install_opener, build_opener
+
 
 def save_images_from_urls(urls, directory, prefix):
-	pass
+	try:
+		os.mkdir(directory)
+	except FileExistsError:
+		pass
+
+	opener = build_opener()
+	opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+	install_opener(opener)
+
+	for i, url in enumerate(urls):
+		try:
+			urlretrieve(url, '{0:s}{1:s}_{2:05d}.png'.format(directory, prefix, i + 1))
+		except (HTTPError, URLError):
+			pass
 
 
 def dl_image_from_google(search, directory='F:/Images/'):
